0a4fbb0577f4b5f18927bce0c45c4a070bf4e02a
XZY <xiezongyuan@xhg.com>
2018-07-18 15:30:54 +0800

    工程初始

---
+++ QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by XZY on 2018/7/18.
+//  Copyright © 2018年 xiezongyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by XZY on 2018/7/18.
+//  Copyright © 2018年 xiezongyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
